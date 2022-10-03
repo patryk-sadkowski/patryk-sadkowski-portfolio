1b675a411e740cda5d7070fe490dc8045a07dfcc
psadkowski <GK5hgw#>
2022-10-03 21:13:53 +0200

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      Lol
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+
+import 'reset.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App/App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
