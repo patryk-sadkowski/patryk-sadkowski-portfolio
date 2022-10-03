0caa9c622fc3a70a5b05ca5fd027cba7af58bdd4
psadkowski <GK5hgw#>
2022-10-03 21:44:47 +0200

    SCSS implementation and basic styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import "./assets/styles/main.scss";
+import "reset.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App";
 
-import 'reset.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App/App'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
